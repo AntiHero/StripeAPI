cca89af2f97b2d71cb5e0bbbad73da0fb97c4cf1
Aleksandr Schemelev <prophreaker@gmail.com>
2023-05-07 21:55:05 +0300

    feat: add prisma disconnect hook and use raw body parser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+  });
+
+  app.enableCors();
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
 }
 bootstrap();
